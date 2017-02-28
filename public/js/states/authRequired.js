dc427e399f75c35c82fc0e98f3b5060cec960e16
Tom Middlehurst <tom.middlehurst@hotmail.co.uk>
2017-02-28 07:24:38 +0000

    writted authRequired state

---
+++ public/js/states/authRequired.js
@@ -0,0 +1,9 @@
+<section>
+	<div>
+			<em>Well this is awkward... You're not allowed to see this</e>
+	</div>
+	<strong>
+		<a href="#" ui-sref="signup">Sign up</a>
+		<a href="#" ui-sref="login">Log in</a>
+	</strong>
+</section>
