3220a8c20e56d051cb38989a26b5c5252e095b06
Tom Middlehurst <tom.middlehurst@hotmail.co.uk>
2017-03-02 14:04:21 +0000

    search state now correctly displays search results, results limited to 15

--- public/js/controllers/movie.ctrl.js
+++ public/js/controllers/movie.ctrl.js
@@ -5,15 +5,19 @@ angular
 function movieController (Movie) {
 	var self = this;
 	self.searchTerm = '';
+	self.movieResults = '';
+
 	self.search = function () {
 		Movie.search({ term: self.searchTerm})
 			.then(function (res) {
 				self.movieResults = JSON.parse(res.data)
 				console.log(self.movieResults)
-				console.log(self.movieResults.results[0].title)
 			})
 			.catch(function (err) {
 				console.log(err)
 			})
+	}
+	self.printMovieResults = function() {
+		console.log(self.movieResults);
 	}
 }
