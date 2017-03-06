4dd3f7b70606fc2c10e68fc6494cc4d52eaa4fec
Tom Middlehurst <tom.middlehurst@hotmail.co.uk>
2017-03-06 11:21:14 +0000

    changes to nav bar links and icons to make actual sense

--- public/js/directives/directives.js
+++ public/js/directives/directives.js
@@ -10,3 +10,13 @@ app.directive('backImg', function(){
         });
     };
 });
+app.directive('scrollOnClick', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, $elm) {
+      $elm.on('click', function() {
+        $("body").animate({scrollTop: $elm.offset().top}, "slow");
+      });
+    }
+  }
+});
